15d8b34a3c6ca07d089817fc14c418296221b2a3
Nada8773 <33834691+Nada8773@users.noreply.github.com>
2018-02-22 01:27:48 +0200

    Update main.c

--- ARM/Embedded Systems - Shape The World/lab9/main.c
+++ ARM/Embedded Systems - Shape The World/lab9/main.c
@@ -1,25 +1,3 @@
-// ***** 0. Documentation Section *****
-// main.c for Lab 9
-// Runs on LM4F120/TM4C123
-// In this lab we are learning functional debugging by dumping
-//   recorded I/O data into a buffer
-// January 15, 2016
-
-// Lab 9
-//      Jon Valvano and Ramesh Yerraballi
-
-// ***** 1. Pre-processor Directives Section *****
-#include "TExaS.h"
-#include "tm4c123gh6pm.h"
-
-// ***** 2. Global Declarations Section *****
-
-// FUNCTION PROTOTYPES: Each subroutine defined
-void DisableInterrupts(void); // Disable interrupts
-void EnableInterrupts(void);  // Enable interrupts
-
-// ***** 3. Subroutines Section *****
-
 
 
 /* 
@@ -34,46 +12,13 @@ For example, if your system detects a change in either PF4 or PF0 input,
 record PortF bits 4,1,0. If your system causes a change in PF1, record PortF bits 4,1,0. 
 
 If both PF4 and PF0 switch are not pressed, the PF1 output should be low.  
-If either PF4 or PF0 switches is pressed, the output toggles at 10 Hz (±10%). 
+If either PF4 or PF0 switches is pressed, the output toggles at 10 Hz (Â±10%). 
 Information collected in the Data array matches the I/O on PortF.
 50 data points are collected only on a change in input or a change in output.
 This means no adjacent elements in the array should be equal.
 
 */
-#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
-#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
-#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
-#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
-#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
-#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
-#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
-#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
-#define PF4                     (*((volatile unsigned long *)0x40025040))
-#define PF3                     (*((volatile unsigned long *)0x40025020))
-#define PF2                     (*((volatile unsigned long *)0x40025010))
-#define PF1                     (*((volatile unsigned long *)0x40025008))
-#define PF0                     (*((volatile unsigned long *)0x40025004))
-#define GPIO_PORTF_DR2R_R       (*((volatile unsigned long *)0x40025500))
-#define GPIO_PORTF_DR4R_R       (*((volatile unsigned long *)0x40025504))
-#define GPIO_PORTF_DR8R_R       (*((volatile unsigned long *)0x40025508))
-#define GPIO_LOCK_KEY           0x4C4F434B  // Unlocks the GPIO_CR register
-#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
-#define NVIC_ST_CURRENT_R       (*((volatile unsigned long *)0xE000E018))
 
-void PortF_Init(void){ volatile unsigned long delay;
-  SYSCTL_RCGC2_R |= 0x00000020;     // 1) activate clock for Port F
-  delay = SYSCTL_RCGC2_R;           // allow time for clock to start
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock GPIO Port F
-  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
-  // only PF0 needs to be unlocked, other bits can't be locked
-  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog on PF
-  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) PCTL GPIO on PF4-0
-  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 in, PF3-1 out
-  GPIO_PORTF_AFSEL_R = 0x00;        // 6) disable alt funct on PF7-0
-  GPIO_PORTF_PUR_R = 0x11;          // enable pull-up on PF0 and PF4
-  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital I/O on PF4-0
-}
 unsigned long SW1;
 unsigned long SW2;
 unsigned long   Led ;
