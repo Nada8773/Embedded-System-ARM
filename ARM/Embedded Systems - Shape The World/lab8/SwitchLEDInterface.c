bdcf9f475143b1b3dbb56c717a9b98778375462d
Nada8773 <33834691+Nada8773@users.noreply.github.com>
2018-02-22 01:28:23 +0200

    Update SwitchLEDInterface.c

--- ARM/Embedded Systems - Shape The World/lab8/SwitchLEDInterface.c
+++ ARM/Embedded Systems - Shape The World/lab8/SwitchLEDInterface.c
@@ -1,29 +1,3 @@
-// ***** 0. Documentation Section *****
-// SwitchLEDInterface.c for Lab 8
-// Runs on LM4F120/TM4C123
-// Use simple programming structures in C to toggle an LED
-// while a button is pressed and turn the LED on when the
-// button is released.  This lab requires external hardware
-// to be wired to the LaunchPad using the prototyping board.
-// January 15, 2016
-//      Jon Valvano and Ramesh Yerraballi
-
-// ***** 1. Pre-processor Directives Section *****
-#include "TExaS.h"
-#include "tm4c123gh6pm.h"
-#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
-#define GPIO_PORTE_DATA_R       (*((volatile unsigned long *)0x400243FC))
-#define GPIO_PORTE_DIR_R        (*((volatile unsigned long *)0x40024400))
-#define GPIO_PORTE_AFSEL_R      (*((volatile unsigned long *)0x40024420))
-#define GPIO_PORTE_DEN_R        (*((volatile unsigned long *)0x4002451C))
-#define GPIO_PORTE_AMSEL_R      (*((volatile unsigned long *)0x40024528))
-#define GPIO_PORTE_PCTL_R       (*((volatile unsigned long *)0x4002452C))
-
-// ***** 2. Global Declarations Section *****
-
-// FUNCTION PROTOTYPES: Each subroutine defined
-void DisableInterrupts(void); // Disable interrupts
-void EnableInterrupts(void);  // Enable interrupts
 
 
 // ***** 3. Subroutines Section *****
