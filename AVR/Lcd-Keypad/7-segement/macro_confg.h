b1bec44c592e64d4771f1d8594db8d9e44649a09
Nada8773 <33834691+Nada8773@users.noreply.github.com>
2018-02-22 01:37:54 +0200

    Add files via upload

---
+++ AVR/Lcd-Keypad/7-segement/macro_confg.h
@@ -0,0 +1,15 @@
+/*
+ * macro_confg.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: ZAS
+ */
+
+#ifndef MACRO_CONFG_H_
+#define MACRO_CONFG_H_
+
+
+#include <stdio.h>
+#include <avr/io.h>
+
+#endif /* MACRO_CONFG_H_ */
