d32dd6f6e91cb9f59ca46262ba067bdc71965ebb
Nada8773 <33834691+Nada8773@users.noreply.github.com>
2018-02-21 23:54:16 +0200

    Add files via upload

---
+++ AVR/Lcd-Keypad/Ex1/common_macros.h
@@ -0,0 +1,13 @@
+/*
+ * common_macros.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: ZAS
+ */
+
+#ifndef COMMON_MACROS_H_
+#define COMMON_MACROS_H_
+
+#define BIT_IS_CLEAR(REG,BIT) (!(REG & (1<<BIT)))
+
+#endif /* COMMON_MACROS_H_ */
