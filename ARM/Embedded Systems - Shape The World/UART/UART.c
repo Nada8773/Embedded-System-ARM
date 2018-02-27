9a7928a4703531efba165f009dc14de0638aad0c
Nada8773 <33834691+Nada8773@users.noreply.github.com>
2018-02-28 00:53:49 +0200

    Add files via upload

--- ARM/Embedded Systems - Shape The World/UART/UART.c
+++ ARM/Embedded Systems - Shape The World/UART/UART.c
@@ -12,55 +12,54 @@
 7- enable digital I/O on PA1-0 
 8- enable alt funct on PA1-0
 9- disable analog functionality on PA
-
 */
-void Init_UART(void)
-{
+void Init_UART(void){
 	SYSCTL_RCGC1 |= SYSCTL_RCGC1_UART0;  
   SYSCTL_RCGC2 |= SYSCTL_RCGC2_GPIOA; 
   UARTCTL &= ~UARTCTL_UARTEN;        
-  UARTIBRD = 27;                    
+  UARTIBRD = 27;   
+  UARTFBRD= 8;
   UARTLCRH= (UARTLCRH_WLEN_8|UARTLCRH_FEN);
   UARTCTL |= UARTCTL_UARTEN;       
 	
   PORTA_AFSEL |= 0x03;           
   PORTA_DEN   |= 0x03;                                                     
-  PORTA_PCTL   = 0x03;//(PORTA_PCTL&0xFFFFFF00)+0x00000011;
-  PORTA_AMSEL &= ~0x03;           
+  PORTA_PCTL   = 0x11;//(PORTA_PCTL&0xFFFFFF00)+0x00000011;
+  PORTA_AMSEL &= ~0x03; 
+}	
 
-}
 
-void UART_sendByte(unsigned char Data)
-{
+
+void UART_sendByte(unsigned char Data){
   while ((UARTFR & UARTFR_TXFF)  !=0);
   UARTDR=Data;
 }
 
 
-unsigned char UART_recieveByte(void)
-{
+
+unsigned char UART_recieveByte(void){
 	while ((UARTFR & UARTFR_RXFE)  !=0);
 	return(UARTDR);
 }
 
 
-void UART_sendString(char *str)
-{
+
+void UART_sendString(char *str){
 	while(*str)
 	{
 		UART_sendByte(*str);
 		str++;
 	}
-	
 }
+	
 
-void UART_recieveString(char *str)
-{
-		*str= UART_recieveByte();
+
+void UART_recieveString(char *str){
+	*str= UART_recieveByte();
 		while (*str !='#')
 		{
 			str++;
 			*str= UART_recieveByte();
 		}
 		*str='\0';
-}
+	}
