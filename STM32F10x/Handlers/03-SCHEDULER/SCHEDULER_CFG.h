c643df0294506053ef308755ff068e4c13621c0a
Nada8773 <nada2478@gmail.com>
2020-03-29 23:49:29 +0200

    ADD SCHEDULER Version 1

---
+++ STM32F10x/Handlers/03-SCHEDULER/SCHEDULER_CFG.h
@@ -0,0 +1,20 @@
+
+/********************************************************************************
+**  FILENAME     : SCHEDULER_CFG.H     			                              **
+**  VERSION      : 1.1                                                        **
+**  DATE         : 16 Mar 2020                                                **                                                                         **
+**  PLATFORM     : STM		                                                  **
+**  AUTHOR       : Nada Mohamed                                               **
+*******************************************************************************/
+
+
+#ifndef SCHEDULER_CFG_H_
+#define SCHEDULER_CFG_H_
+
+/* number of tasks to run */
+#define NUMBER_OF_TASKS     2
+
+/* number of ticks in ms */
+#define SYSTICK_TICKMS      1000U
+
+#endif
