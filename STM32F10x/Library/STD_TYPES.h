70ee936f28d4b2e9f4b8f8eb3d18ede4eebbc387
Nada8773 <nada2478@gmail.com>
2020-03-30 00:44:31 +0200

    add library

---
+++ STM32F10x/Library/STD_TYPES.h
@@ -0,0 +1,34 @@
+
+ /****************************************************************
+ * Author  : Nada Mohamed 
+ * Data    : 18 December ‎2019
+ * Version : v01
+ *****************************************************************/
+
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef  unsigned char           u8 ; 
+typedef  unsigned short int      u16;
+typedef  unsigned long int       u32;
+									
+typedef  signed char             s8 ;
+typedef  signed short int        s16;
+typedef  signed long int         s32;
+									
+typedef  float                   f32;
+typedef  double                  f64;
+typedef  long double             f96;
+	  
+
+typedef u8          Std_ReturnType;
+
+#define E_OK        (Std_ReturnType)0
+#define E_NOT_OK    (Std_ReturnType)1
+#define E_BUSY      (Std_ReturnType)2
+
+#define NULL ((void *)0)
+
+#endif	  
+	  
+	  
